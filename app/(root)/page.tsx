a739cd6ea675e808d564c52adb53bbec26944daf
zheng <zhengchuyi93@gmail.com>
2023-12-29 18:55:08 -0800

    first commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -35,8 +35,8 @@ export default function Home() {
     <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
     <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
     <div className="flex w-full flex-col gap-5 md:flex-row">
-          <Search />
-          <CategoryFilter />
+          {/* <Search />
+          <CategoryFilter /> */}
         </div>
     </section>
     </>
